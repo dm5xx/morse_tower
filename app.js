f38f31dc695f670b85b3f0d14b02883a228e438d
Unknown <dm5xx@gmx.de>
2018-04-30 22:28:01 +0200

    fixes...

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ var counter = 0;
 var hours = (new Date()).getHours();
 var isDayTime = hours > 4 && hours < 18;
 
-while(!isDayTime)
+while(1)
 {
-    if(counter < 5)
+    if(!isDayTime)
     {
-        morseText("5  5  5  5  5  ", 50000);
-        counter++;
+        if(counter < 5)
+        {
+            morseText("5  5  5  5  5  ", 50000);
+            counter++;
+        }
+        else
+        {
+            morseText("ol7m  ol7m", 120000);
+        counter = 0;
+        }
+        morseText('     ', 100000);
     }
-    else
-    {
-        morseText("ol7m  ol7m", 120000);
-       counter = 0;
-    }
-    morseText('     ', 100000);
     hours = (new Date()).getHours();
     isDayTime = hours > 4 && hours < 18;
 }
